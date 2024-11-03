67947b74b85b32c1bc5cadd4252e761b33438810
AcinonyxPardalis <getsironis@gmail.com>
2024-11-03 18:52:11 +0200

    Week03 Homework
    Completed week 3 but did not complete lucid. I have only run npm install so node_modules is created.

--- code/Week03/homework/Homework1.hs
+++ code/Week03/homework/Homework1.hs
@@ -7,12 +7,20 @@
 
 module Homework1 where
 
+import           Plutus.V1.Ledger.Interval (contains)
 import           Plutus.V2.Ledger.Api (BuiltinData, POSIXTime, PubKeyHash,
-                                       ScriptContext, Validator,
-                                       mkValidatorScript)
+                                       ScriptContext (scriptContextTxInfo), Validator, 
+                                       TxInfo (txInfoValidRange),
+                                       from, to, mkValidatorScript)
+import           Plutus.V2.Ledger.Contexts (txSignedBy)
 import           PlutusTx             (compile, unstableMakeIsData)
-import           PlutusTx.Prelude     (Bool (..))
-import           Utilities            (wrapValidator)
+import           PlutusTx.Prelude     (Bool, traceIfFalse, ($), (&&), (+), (||))
+import           Prelude                   (IO, String)
+import           Utilities            (Network, posixTimeFromIso8601,
+                                            printDataToJSON,
+                                            validatorAddressBech32,
+                                            wrapValidator, writeValidatorToFile)
+import Data.Aeson (Value(Bool))
 
 ---------------------------------------------------------------------------------------------------
 ----------------------------------- ON-CHAIN / VALIDATOR ------------------------------------------
@@ -29,7 +37,24 @@ unstableMakeIsData ''VestingDatum
 -- This should validate if either beneficiary1 has signed the transaction and the current slot is before or at the deadline
 -- or if beneficiary2 has signed the transaction and the deadline has passed.
 mkVestingValidator :: VestingDatum -> () -> ScriptContext -> Bool
-mkVestingValidator _dat () _ctx = False -- FIX ME!
+mkVestingValidator _dat () _ctx = traceIfFalse "beneficiary's signature missing" signedByBeneficiary &&
+                                  if signedByFirst then traceIfFalse "contract expired for the first beneficiary" deadlineExpired
+                                  else traceIfFalse "deadline not reached for the second beneficiary" deadlineReached
+  where
+    info :: TxInfo
+    info = scriptContextTxInfo _ctx
+
+    signedByBeneficiary :: Bool
+    signedByBeneficiary = txSignedBy info (beneficiary1 _dat) || txSignedBy info (beneficiary2 _dat)
+
+    signedByFirst :: Bool
+    signedByFirst = txSignedBy info $ beneficiary1 _dat
+
+    deadlineReached :: Bool
+    deadlineReached = contains (from $ (deadline _dat + 1)) $ txInfoValidRange info
+
+    deadlineExpired :: Bool
+    deadlineExpired = contains (to $ deadline _dat) $ txInfoValidRange info
 
 {-# INLINABLE  mkWrappedVestingValidator #-}
 mkWrappedVestingValidator :: BuiltinData -> BuiltinData -> BuiltinData -> ()
